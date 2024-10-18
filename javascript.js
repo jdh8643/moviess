caeb1da5b57f012ea667de0fb8de0db1b2f6f824
jdh8643 <156303898+jdh8643@users.noreply.github.com>
2024-10-18 19:25:17 +0900

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const API_KEY = 'api_key=3e8cb92173300dc3cd1ec1811185bc7a'
+const API_KEY = 'api_key=3e8cb92173300dc3cd1ec1811185bc7a&language=ko&page=1'
 const BASE_URL = 'https://api.themoviedb.org/3'
 const API_URL = BASE_URL + '/movie/popular?' + API_KEY;
 const IMG_URL ='https://image.tmdb.org/t/p/w500'
